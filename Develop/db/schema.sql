cd13a3b53927fb221eb3cf7c94f3d1d0a57982a5
Victor Marczyk <marczykv@gmail.com>
2023-11-10 22:57:28 -0500

    schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,8 +1,30 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+--  Category table
+CREATE TABLE Category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+--  Product table
+CREATE TABLE Product (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES Category(id)
+);
 
-USE ecommerce_db;
+-- Tag table
+CREATE TABLE Tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(50) NOT NULL
+);
 
+-- establish relationship between Product and Tag
+CREATE TABLE ProductTag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES Product(id),
+  FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
--- Develop/server.js
+++ Develop/server.js
@@ -5,14 +5,12 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware to parse JSON and urlencoded form data
+// Middleware to parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Use the main routes module
 app.use('/api', routes);  
 
-// Sync Sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
